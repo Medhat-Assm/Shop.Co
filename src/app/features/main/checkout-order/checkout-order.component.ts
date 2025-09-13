6c46f89aa2a0feed9c5e237a9657c88977e8786d
Medhat <medhatassem.dev@gmail.com>
2025-09-13 03:47:12 +0300

    update folder structures

---
+++ src/app/features/main/checkout-order/checkout-order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-order',
+  imports: [],
+  templateUrl: './checkout-order.component.html',
+  styleUrl: './checkout-order.component.scss'
+})
+export class CheckoutOrderComponent {
+
+}
