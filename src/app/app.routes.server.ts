2fc8cd24a0d4555d427df74177073fd4d2d04015
Medhat <medhatassem.dev@gmail.com>
2025-09-17 08:58:31 +0300

    update routes server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'product-details/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'order/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'verification-code',
+    renderMode: RenderMode.Client,
+  },
 ];
